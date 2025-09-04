22a262709e5cd5df0c08d017005bd81fa5a208b8
KorolER <elizavetakorol2005@gmail.com>
2025-09-04 22:51:42 +0300

    fix: SmartMergeRegistrator

--- Obscura/Assets/SmartMergeRegistrator.cs
+++ Obscura/Assets/SmartMergeRegistrator.cs
@@ -3,43 +3,52 @@ using UnityEngine;
 using UnityEditor;
 using System;
 
-namespace GitIntegration
-{
+namespace GitIntegration {
     [InitializeOnLoad]
-    public class SmartMergeRegistrator
-    {
+    public class SmartMergeRegistrator {
         const string SmartMergeRegistratorEditorPrefsKey = "smart_merge_installed";
         const int Version = 1;
         static string VersionKey = $"{Version}_{Application.unityVersion}";
 
+        public static string ExecuteGitWithParams(string param) {
+            var processInfo = new System.Diagnostics.ProcessStartInfo("git");
+
+            processInfo.UseShellExecute = false;
+            processInfo.WorkingDirectory = Environment.CurrentDirectory;
+            processInfo.RedirectStandardOutput = true;
+            processInfo.RedirectStandardError = true;
+            processInfo.CreateNoWindow = true;
+
+            var process = new System.Diagnostics.Process();
+            process.StartInfo = processInfo;
+            process.StartInfo.FileName = "git";
+            process.StartInfo.Arguments = param;
+            process.Start();
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+                throw new Exception(process.StandardError.ReadLine());
+
+            return process.StandardOutput.ReadLine();
+        }
+
         [MenuItem("Tools/Git/SmartMerge registration")]
-        static void SmartMergeRegister()
-        {
-            try
-            {
+        static void SmartMergeRegister() {
+            try {
                 var UnityYAMLMergePath = EditorApplication.applicationContentsPath + "/Tools" + "/UnityYAMLMerge.exe";
-                Utils.ExecuteGitWithParams("config merge.unityyamlmerge.name \"Unity SmartMerge (UnityYamlMerge)\"");
-                Utils.ExecuteGitWithParams($"config merge.unityyamlmerge.driver \"\\\"{UnityYAMLMergePath}\\\" merge -h -p --force --fallback none %O %B %A %A\"");
-                Utils.ExecuteGitWithParams("config merge.unityyamlmerge.recursive binary");
+                ExecuteGitWithParams("config merge.unityyamlmerge.name \"Unity SmartMerge (UnityYamlMerge)\"");
+                ExecuteGitWithParams($"config merge.unityyamlmerge.driver \"\\\"{UnityYAMLMergePath}\\\" merge -h -p --force --fallback none %O %B %A %A\"");
+                ExecuteGitWithParams("config merge.unityyamlmerge.recursive binary");
                 EditorPrefs.SetString(SmartMergeRegistratorEditorPrefsKey, VersionKey);
                 Debug.Log($"Succesfuly registered UnityYAMLMerge with path {UnityYAMLMergePath}");
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Debug.Log($"Fail to register UnityYAMLMerge with error: {e}");
             }
         }
 
-        [MenuItem("Tools/Git/SmartMerge unregistration")]
-        static void SmartMergeUnRegister()
-        {
-            Utils.ExecuteGitWithParams("config --remove-section merge.unityyamlmerge");
-            Debug.Log($"Succesfuly unregistered UnityYAMLMerge");
-        }
-
         //Unity calls the static constructor when the engine opens
-        static SmartMergeRegistrator()
-        {
+        static SmartMergeRegistrator() {
             var instaledVersionKey = EditorPrefs.GetString(SmartMergeRegistratorEditorPrefsKey);
             if (instaledVersionKey != VersionKey)
                 SmartMergeRegister();
