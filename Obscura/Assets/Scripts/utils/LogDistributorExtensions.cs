6103b90b14d6244559eb73b521ad37dd8596ff3d
Usevalad Buben <usevaladbuben@gmail.com>
2026-02-22 00:45:58 +0300

    Add LogDistributorExtensions class that provide methods to make logs by ILogDistributor implementers

---
+++ Obscura/Assets/Scripts/utils/LogDistributorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public static class LogDistributorExtensions
+{
+    private static string GetStandardMessage(ILogDistributor distributor, string message)
+    {
+        return $"[{distributor.DistributorName}] - {message}.";
+    }
+
+    private static string GetTimeMessage(ILogDistributor distributor, string message)
+    {
+        return $"[{Time.unscaledTime}] - {GetStandardMessage(distributor, message)}.)";
+    }
+    
+    public static void Log(this ILogDistributor distributor, string message)
+    {
+        Debug.Log(GetStandardMessage(distributor, message));
+    }
+
+    public static void LogTime(this ILogDistributor distributor, string message)
+    {
+        Debug.Log(GetTimeMessage(distributor, message));
+    }
+}
