dffc6994819d064de74e287aa76dbdaaef1ca937
Usevalad Buben <usevaladbuben@gmail.com>
2026-02-22 20:29:01 +0300

    [IMPORTANT] AbstractTile: add new method "OnThisCurrentEvent" that operates with a plethora of next cells. Some crutches... :[

--- Obscura/Assets/Scripts/Level tiles/AbstractTile.cs
+++ Obscura/Assets/Scripts/Level tiles/AbstractTile.cs
@@ -1,4 +1,6 @@
 
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class AbstractTile: MonoBehaviour {
@@ -8,4 +10,16 @@ public abstract class AbstractTile: MonoBehaviour {
     public abstract bool CheckIsCurrentObject(Vector3Int cellPos);
     public abstract void OnThisNextEvent(GameObject trigger);
     public abstract void OnThisCurrentEvent(AbstractTile nextCell, GameObject trigger);
+
+    public void OnThisCurrentEvents(List<AbstractTile> nextCells, GameObject trigger)
+    {
+        // In some conditions it is needed to run with null (ex. PortalTile)
+        if (!nextCells.Any())
+        {
+            OnThisCurrentEvent(null, trigger);
+            return;
+        }
+        
+        nextCells.ForEach(x => OnThisCurrentEvent(x, trigger));
+    }
 }
