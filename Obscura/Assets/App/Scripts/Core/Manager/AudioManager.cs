85d8772b03cc0bf951d2f2e1e9a3f12c1077ce57
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 23:03:18 +0300

    Remove useless Start method (logic in the ButtonTogglerGlobalSounds do this thing)

--- Obscura/Assets/App/Scripts/Core/Manager/AudioManager.cs
+++ Obscura/Assets/App/Scripts/Core/Manager/AudioManager.cs
@@ -6,20 +6,6 @@ public class AudioManager : MonoBehaviour, ILogDistributor {
 
     [SerializeField] private AudioMixer audioMixer;
 
-    private const string OSTVolume = "OSTVolume";
-    private const string SFXVolume = "SFXVolume";
-
-    private void Start() {
-        this.Log("Awake in scene: " + gameObject.scene.name);
-        string[] soundVolumePrefsNames = { OSTVolume, SFXVolume };
-
-        foreach (string soundVolumePref in soundVolumePrefsNames) {
-            bool turnedOn = PlayerPrefs.GetInt(soundVolumePref, 1) == 1;
-            this.Log($"{soundVolumePref} active: {turnedOn}");
-            setVolume(soundVolumePref, turnedOn);
-        }
-    }
-
     public void setVolume(string name, bool turnedOn) {
         float volume = turnedOn ? 0.0f : -80f;
         audioMixer.SetFloat(name, volume);
