4542a0fd56999316f92bac2dd6ac456c0a2d4e0f
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 14:44:30 +0300

    Create EnergyConfig scriptable object

---
+++ Obscura/Assets/App/Scripts/Core/Energy/EnergyConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace App.Scripts.Core.Energy
+{
+    public class EnergyConfig : ScriptableObject
+    {
+        [SerializeField] private int _maxCount = 5;
+        [SerializeField] private float _recoverSpeed = 300f;
+
+        public int MaxCount => _maxCount;
+        public float RecoverSpeed => _recoverSpeed;
+    }
+}
