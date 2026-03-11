b129324cc26d08bd390d9d414851762713a98de6
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 15:17:45 +0300

    Add energy entity class

---
+++ Obscura/Assets/App/Scripts/Core/Storage/Entities/Energy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using App.Scripts.Core.Storage.Dto;
+
+namespace App.Scripts.Core.Storage.Entities
+{
+    public sealed class Energy : IEntity<EnergyDto>
+    {
+        public int Count { get; set; }
+        public DateTime ReductionDateTime { get; set; }
+        
+        public EnergyDto ToDto()
+        {
+            return new EnergyDto()
+            {
+                Count = Count,
+                ReductionDateTime = ReductionDateTime
+            };
+        }
+    }
+}
