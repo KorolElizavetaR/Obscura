327b0878ff9d66d8a1d3e1f2696d728d6d80eb50
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 15:59:04 +0300

    Add dictionary as storage

--- Obscura/Assets/App/Scripts/Core/Storage/Core/BaseStorage.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/Core/BaseStorage.cs
@@ -1,7 +1,12 @@
-﻿namespace App.Scripts.Core.Storage.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace App.Scripts.Core.Storage.Core
 {
     public class BaseStorage : IStorage
     {
+        private readonly Dictionary<Type, object> _storage = new ();
+        
         public void Load()
         {
             throw new System.NotImplementedException();
