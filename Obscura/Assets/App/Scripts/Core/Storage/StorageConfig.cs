0cb8721c2e191f31e88e1b6d88e74f80d3f4338a
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 15:25:10 +0300

    Add _currentLevelIdentifier transcription

--- Obscura/Assets/App/Scripts/Core/Storage/StorageConfig.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/StorageConfig.cs
@@ -4,6 +4,8 @@ namespace App.Scripts.Core.Storage
 {
     public class StorageConfig : ScriptableObject
     {
+        [SerializeField] private string _currentlevelIdentifier = "level";
         
+        public string CurrentLevelIdentifier => _currentlevelIdentifier;
     }
 }
