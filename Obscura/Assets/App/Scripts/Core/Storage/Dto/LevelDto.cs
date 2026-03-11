2afa4bb1a8ba3f722b1bfb4e55a0b68dce248468
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 15:28:00 +0300

    Create level dto class

---
+++ Obscura/Assets/App/Scripts/Core/Storage/Dto/LevelDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace App.Scripts.Core.Storage.Dto
+{
+    [Serializable]
+    public class LevelDto : IDto
+    {
+        private int _id;
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+    }
+}
