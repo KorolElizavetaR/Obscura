e85768cdce7fcc5f072ac236548bb381fe13a7ba
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 17:54:19 +0300

    Simplify fields and properties of dto classes

--- Obscura/Assets/App/Scripts/Core/Storage/Dto/EnergyDto.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/Dto/EnergyDto.cs
@@ -5,19 +5,7 @@ namespace App.Scripts.Core.Storage.Dto
     [Serializable]
     public sealed class EnergyDto : IDto
     {
-        private int _count;
-        private DateTime _reductionDateTime;
-
-        public int Count
-        {
-            get => _count;
-            set => _count = value;
-        }
-
-        public DateTime ReductionDateTime
-        {
-            get => _reductionDateTime;
-            set => _reductionDateTime = value;
-        }
+        public int Count;
+        public DateTime ReductionDateTime;
     }
 }
