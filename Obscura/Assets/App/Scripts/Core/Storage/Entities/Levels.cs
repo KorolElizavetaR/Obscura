f96cc366eed3b3376fb33f493a6d7d785efacaeb
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 22:49:26 +0300

    Expand level entity class with list of completed levels

--- Obscura/Assets/App/Scripts/Core/Storage/Entities/Levels.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/Entities/Levels.cs
@@ -1,23 +1,27 @@
-﻿using App.Scripts.Core.Storage.Dto;
+﻿using System.Collections.Generic;
+using App.Scripts.Core.Storage.Dto;
 
 namespace App.Scripts.Core.Storage.Entities
 {
     public class Levels : IEntity<LevelsDto>
     {
         public int CurrentLevelId { get; set; }
+        public List<int> CompletedLevels { get; set; }
 
         public Levels() {}
         
         public Levels(LevelsDto dto)
         {
             CurrentLevelId = dto.CurrentLevelId;
+            CompletedLevels = dto.CompletedLevels;
         }
         
         public LevelsDto ToDto()
         {
             return new LevelsDto()
             {
-                CurrentLevelId = CurrentLevelId
+                CurrentLevelId = CurrentLevelId,
+                CompletedLevels = CompletedLevels
             };
         }
     }
