778bc122f15f181b93b96b3d55ee37424f868d99
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 16:46:28 +0300

    Add override for Save method at EntitiesStorage

--- Obscura/Assets/App/Scripts/Core/Storage/EntitiesStorage.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/EntitiesStorage.cs
@@ -10,5 +10,18 @@ namespace App.Scripts.Core.Storage
             TryAdd(PlayerPrefsEx.GetJson<Entities.Level>(StorageContracts.CurrentLevel));
             TryAdd(PlayerPrefsEx.GetJson<Entities.Energy>(StorageContracts.Energy));
         }
+
+        public override void Save()
+        {
+            if (TryGet(out Entities.Level level))
+            {
+                PlayerPrefsEx.SetJson(StorageContracts.CurrentLevel, level);
+            }
+            
+            if (TryGet(out Entities.Energy energy))
+            {
+                PlayerPrefsEx.SetJson(StorageContracts.Energy, energy);
+            }
+        }
     }
 }
