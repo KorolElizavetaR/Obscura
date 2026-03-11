4a912a605314d0a16edc62a47bdb87b003fdd34b
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 17:32:19 +0300

    Add constructor to energy entity

--- Obscura/Assets/App/Scripts/Core/Storage/Entities/Energy.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/Entities/Energy.cs
@@ -7,6 +7,14 @@ namespace App.Scripts.Core.Storage.Entities
     {
         public int Count { get; set; }
         public DateTime ReductionDateTime { get; set; }
+
+        public Energy() {}
+        
+        public Energy(EnergyDto dto)
+        {
+            Count = dto.Count;
+            ReductionDateTime = dto.ReductionDateTime;
+        }
         
         public EnergyDto ToDto()
         {
