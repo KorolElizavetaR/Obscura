59065693cd028897f8fb8031e5c9d1f8c5b5d1da
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 15:28:09 +0300

    Create level entity file

---
+++ Obscura/Assets/App/Scripts/Core/Storage/Entities/Level.cs
@@ -0,0 +1,14 @@
+﻿using App.Scripts.Core.Storage.Dto;
+
+namespace App.Scripts.Core.Storage.Entities
+{
+    public class Level : IEntity<LevelDto>
+    {
+        public int Id { get; set; }
+        
+        public LevelDto ToDto()
+        {
+            return new LevelDto();
+        }
+    }
+}
