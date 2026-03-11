efaf3b61ea5898e8a32a3311d053a353b5fb63c8
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 18:59:05 +0300

    Replace old toggle state usages with new methods

--- Obscura/Assets/App/Scripts/Core/UI/button/ButtonToggler.cs
+++ Obscura/Assets/App/Scripts/Core/UI/button/ButtonToggler.cs
@@ -13,11 +13,8 @@ public abstract class ButtonToggler : MonoBehaviour, ILogDistributor {
     }
     
     public string DistributorName => GetType().Name;
-
-    [SerializeField] protected string prefName;
     
     private Image[] buttonImages;
-    protected bool currentToggleState;
 
     private ButtonTogglers _buttonTogglersEntity;
     [SerializeField] private ToggleType _toggleType;
@@ -26,21 +23,17 @@ public abstract class ButtonToggler : MonoBehaviour, ILogDistributor {
         EntitiesStorage.Instance.TryGet(out _buttonTogglersEntity);
         
         buttonImages = GetComponentsInChildren<Image>();
-        currentToggleState = PlayerPrefs.GetInt(prefName, 1) == 1;
-        this.Log($"INIT currentToggleState for pref {prefName}: {currentToggleState}");
         changeImageAlpha();
     }
 
-    public virtual void toggleState() {
-        currentToggleState = !currentToggleState;
-        this.Log($"currentToggleState for pref {prefName}: {currentToggleState}");
+    public virtual void toggleState()
+    {
+        ChangeToggleState();
         changeImageAlpha();
-        PlayerPrefs.SetInt(prefName, currentToggleState ? 1 : 0);
-        PlayerPrefs.Save();
     }
 
     public void changeImageAlpha() {
-        float targetAlpha = currentToggleState ? 1f : 0.5f;
+        float targetAlpha = GetToggleState() ? 1f : 0.5f;
 
         foreach (Image img in buttonImages) {
             img.color = new Color(img.color.r, img.color.g, img.color.b, targetAlpha);
