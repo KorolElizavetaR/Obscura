eb66d3f107a75fa3057696fad3442090f369d221
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 14:36:54 +0300

    Create energy dto

---
+++ Obscura/Assets/App/Scripts/Core/Storage/Dto/EnergyDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace App.Scripts.Core.Storage.Dto
+{
+    [Serializable]
+    public sealed class EnergyDto
+    {
+        private int _energy;
+        private DateTime _reductionDateTime;
+    }
+}
