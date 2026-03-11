dfacb3615a348d0e591cfda2864f124cc8c2655d
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 23:00:52 +0300

    Extract method for volume setup

--- Obscura/Assets/App/Scripts/Core/UI/button/ButtonTogglerGlobalSounds.cs
+++ Obscura/Assets/App/Scripts/Core/UI/button/ButtonTogglerGlobalSounds.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class ButtonTogglerGlobalSounds : ButtonToggler {
     [SerializeField]
     private AudioManager audioManager;
-
-    public override void toggleState() {
+    
+    public override void toggleState()
+    {
         base.toggleState();
+        SetVolume();
+    }
+
+    private void SetVolume()
+    {
         audioManager.setVolume(GetToggleName(), GetToggleState());
     }
 }
