a11d2aaff0b08a940ee4adffc43022b4d56f19b9
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 17:56:57 +0300

    Fix loading and saving: instead using entity use dto classes

--- Obscura/Assets/App/Scripts/Core/Storage/EntitiesStorage.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/EntitiesStorage.cs
@@ -1,4 +1,6 @@
 ﻿using App.Scripts.Core.Storage.Core;
+using App.Scripts.Core.Storage.Dto;
+using App.Scripts.Core.Storage.Entities;
 using Extensions.Unity.PlayerPrefsEx;
 
 namespace App.Scripts.Core.Storage
@@ -7,20 +9,20 @@ namespace App.Scripts.Core.Storage
     {
         public override void Load()
         {
-            TryAdd(PlayerPrefsEx.GetJson<Entities.Level>(StorageContracts.CurrentLevel));
-            TryAdd(PlayerPrefsEx.GetJson<Entities.Energy>(StorageContracts.Energy));
+            TryAdd(new Level(PlayerPrefsEx.GetJson(StorageContracts.CurrentLevel, new LevelDto())));
+            TryAdd(new Entities.Energy(PlayerPrefsEx.GetJson(StorageContracts.Energy, new EnergyDto())));
         }
 
         public override void Save()
         {
             if (TryGet(out Entities.Level level))
             {
-                PlayerPrefsEx.SetJson(StorageContracts.CurrentLevel, level);
+                PlayerPrefsEx.SetJson(StorageContracts.CurrentLevel, level.ToDto());
             }
             
             if (TryGet(out Entities.Energy energy))
             {
-                PlayerPrefsEx.SetJson(StorageContracts.Energy, energy);
+                PlayerPrefsEx.SetJson(StorageContracts.Energy, energy.ToDto());
             }
         }
     }
