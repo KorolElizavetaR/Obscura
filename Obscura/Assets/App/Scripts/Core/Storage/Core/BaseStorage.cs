a1bc8f11aacbb2d336e44296fc31f7b48e0ca2ff
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 15:42:09 +0300

    Implement IStorage interface to BaseStorage

--- Obscura/Assets/App/Scripts/Core/Storage/Core/BaseStorage.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/Core/BaseStorage.cs
@@ -1,7 +1,20 @@
 ﻿namespace App.Scripts.Core.Storage.Core
 {
-    public class BaseStorage
+    public class BaseStorage<T> : IStorage<T>
     {
-        
+        public void Load()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryGet(out T value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
