ff57f527e61b7d2cdefe5abc99698bceb6ba138f
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 18:19:24 +0300

    Cleanup the code

--- Obscura/Assets/App/Scripts/Core/UI/modal windows/ModalLevelSelection.cs
+++ Obscura/Assets/App/Scripts/Core/UI/modal windows/ModalLevelSelection.cs
@@ -3,26 +3,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ModalLevelSelection : ModalEventHandler {
-
-
-    [SerializeField]
-    TextMeshProUGUI text;
-
-    //void Start() {
-    //    text = levelText.GetComponent<TextMeshPro>();
-    //    if (text == null) {
-    //        Debug.LogError("TextMeshPro component not found on levelText GameObject!");
-    //    }
-    //}
+    [SerializeField] TextMeshProUGUI text;
 
     public override void onClickOpenModal() {
         int levelNum = PlayerPrefs.GetInt("level", 1);
-        text.text = "Уровень " + levelNum;
+        text.text = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ " + levelNum;
         base.onClickOpenModal();
     }
 
     public void loadLevel() {
         SceneManager.LoadScene("game_scene");
     }
-
 }
