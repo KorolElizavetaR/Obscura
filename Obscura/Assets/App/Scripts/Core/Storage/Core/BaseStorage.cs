1dd9d33f5b6f7d4a9780ebd6343b98286cddc764
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 16:13:03 +0300

    Define TryGet method for BaseStorage

--- Obscura/Assets/App/Scripts/Core/Storage/Core/BaseStorage.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/Core/BaseStorage.cs
@@ -13,8 +13,23 @@ namespace App.Scripts.Core.Storage.Core
         }
 
         public bool TryGet<T>(out T value)
-        { 
-            throw new System.NotImplementedException();
+        {
+            value = default;
+            
+            if (!_storage.TryGetValue(typeof(T), out var foundValue))
+            {
+                return false;
+            }
+
+            if (foundValue is not T valueConverted)
+            {
+                _storage.Remove(typeof(T));
+                return false;
+            }
+
+            value = valueConverted;
+
+            return true;
         }
 
         public void Save()
