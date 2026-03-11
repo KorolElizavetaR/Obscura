64843a25f42edc39d8126cbecfb7e8bae91f8f48
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 16:44:12 +0300

    Add override for Load method at EntitiesStorage

--- Obscura/Assets/App/Scripts/Core/Storage/EntitiesStorage.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/EntitiesStorage.cs
@@ -1,9 +1,14 @@
 ﻿using App.Scripts.Core.Storage.Core;
+using Extensions.Unity.PlayerPrefsEx;
 
 namespace App.Scripts.Core.Storage
 {
     public class EntitiesStorage : BaseStorage<EntitiesStorage>
     {
-        
+        public override void Load()
+        {
+            TryAdd(PlayerPrefsEx.GetJson<Entities.Level>(StorageContracts.CurrentLevel));
+            TryAdd(PlayerPrefsEx.GetJson<Entities.Energy>(StorageContracts.Energy));
+        }
     }
 }
