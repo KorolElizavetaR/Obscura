11415fa8acae8448141701e8aa3298f819a6653c
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 18:32:37 +0300

    Create ButtonTogglersDto class

---
+++ Obscura/Assets/App/Scripts/Core/Storage/Dto/ButtonTogglersDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace App.Scripts.Core.Storage.Dto
+{
+    [Serializable]
+    public sealed class ButtonTogglersDto : IDto
+    {
+        private bool _ostVolumeEnabled = true;
+        private bool _sfxVolumeEnabled = true;
+
+        public bool OstVolumeEnabled
+        {
+            get => _ostVolumeEnabled;
+            set => _ostVolumeEnabled = value;
+        }
+
+        public bool SfxVolumeEnabled
+        {
+            get => _sfxVolumeEnabled;
+            set => _sfxVolumeEnabled = value;
+        }
+    }
+}
