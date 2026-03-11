71055f1402761d107a1d256d0eb56769de111911
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 15:40:57 +0300

    Implement IStorage interface to BaseMonoStorage

--- Obscura/Assets/App/Scripts/Core/Storage/Core/BaseMonoStorage.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/Core/BaseMonoStorage.cs
@@ -2,8 +2,21 @@
 
 namespace App.Scripts.Core.Storage.Core
 {
-    public class BaseMonoStorage : MonoBehaviour
+    public class BaseMonoStorage<T> : MonoBehaviour, IStorage<T>
     {
-        
+        public void Load()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryGet(out T value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
