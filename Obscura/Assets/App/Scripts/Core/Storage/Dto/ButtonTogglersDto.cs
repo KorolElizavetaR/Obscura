139034780a87ebe0d3715aea043b96b567a82136
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-11 19:25:31 +0300

    Reformat all dto classes to math code JsonConverter style

--- Obscura/Assets/App/Scripts/Core/Storage/Dto/ButtonTogglersDto.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/Dto/ButtonTogglersDto.cs
@@ -5,19 +5,7 @@ namespace App.Scripts.Core.Storage.Dto
     [Serializable]
     public sealed class ButtonTogglersDto : IDto
     {
-        private bool _ostVolumeEnabled = true;
-        private bool _sfxVolumeEnabled = true;
-
-        public bool OstVolumeEnabled
-        {
-            get => _ostVolumeEnabled;
-            set => _ostVolumeEnabled = value;
-        }
-
-        public bool SfxVolumeEnabled
-        {
-            get => _sfxVolumeEnabled;
-            set => _sfxVolumeEnabled = value;
-        }
+        public bool OstVolumeEnabled;
+        public bool SfxVolumeEnabled;
     }
 }
--- Obscura/Assets/App/Scripts/Core/Storage/Dto/LevelDto.cs
+++ Obscura/Assets/App/Scripts/Core/Storage/Dto/LevelDto.cs
@@ -5,12 +5,6 @@ namespace App.Scripts.Core.Storage.Dto
     [Serializable]
     public class LevelDto : IDto
     {
-        private int _id;
-
-        public int Id
-        {
-            get => _id;
-            set => _id = value;
-        }
+        public int Id;
     }
 }
