3bc991803a72363a4e6162ee7f115ea639f0c4ce
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-12 11:33:15 +0300

    Delete EnergyUpdater direct energy entity usage

--- Obscura/Assets/App/Scripts/Core/Energy/EnergyUpdater.cs
+++ Obscura/Assets/App/Scripts/Core/Energy/EnergyUpdater.cs
@@ -1,16 +1,14 @@
-﻿using App.Scripts.Core.Storage;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace App.Scripts.Core.Energy
 {
     public class EnergyUpdater : MonoBehaviour
     {
         [SerializeField] private EnergyConfig _energyConfig;
-        private Storage.Entities.Energy _energyEntity;
         
         protected virtual void Awake()
         {
-            EntitiesStorage.Instance.TryGet(out _energyEntity);
+            
         }
     }
 }
