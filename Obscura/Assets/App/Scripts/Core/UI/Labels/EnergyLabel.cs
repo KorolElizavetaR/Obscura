b3eb0bb902ad96be5505d52b1ee3721bb8246a11
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-12 12:12:52 +0300

    Assign energy entity at the awake method

--- Obscura/Assets/App/Scripts/Core/UI/Labels/EnergyLabel.cs
+++ Obscura/Assets/App/Scripts/Core/UI/Labels/EnergyLabel.cs
@@ -1,4 +1,5 @@
 ﻿using App.Scripts.Core.Energy;
+using App.Scripts.Core.Storage;
 using TMPro;
 using UnityEngine;
 
@@ -11,5 +12,10 @@ namespace App.Scripts.Core.UI.Labels
         
         private EnergyConfig _energyConfig;
         private Storage.Entities.Energy _energyEntity;
+
+        protected virtual void Awake()
+        {
+            EntitiesStorage.Instance.TryGet(out _energyEntity);
+        }
     }
 }
