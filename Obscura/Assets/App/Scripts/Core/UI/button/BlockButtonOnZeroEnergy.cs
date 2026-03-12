1136ef966d1d7dc5b68b957a6ea328e0cb5049ca
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-12 16:27:34 +0300

    Add energy entity field and assignification

--- Obscura/Assets/App/Scripts/Core/UI/button/BlockButtonOnZeroEnergy.cs
+++ Obscura/Assets/App/Scripts/Core/UI/button/BlockButtonOnZeroEnergy.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using App.Scripts.Core.Storage;
+using UnityEngine;
 
 namespace App.Scripts.Core.UI.button
 {
     public class BlockButtonOnZeroEnergy : MonoBehaviour
     {
-        
+        private Storage.Entities.Energy _energyEntity;
+
+        protected virtual void Awake()
+        {
+            EntitiesStorage.Instance.TryGet(out _energyEntity);
+        }
     }
 }
