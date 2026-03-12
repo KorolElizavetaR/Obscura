bff7967e54ce7a1aa9709a3d6c5363b52278edf5
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-12 16:17:17 +0300

    Add try find method to find list of components

--- Obscura/Assets/App/Scripts/Core/Utils/Finder.cs
+++ Obscura/Assets/App/Scripts/Core/Utils/Finder.cs
@@ -1,4 +1,8 @@
-﻿namespace App.Scripts.Core.Utils
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace App.Scripts.Core.Utils
 {
     public static class Finder
     {
@@ -9,5 +13,18 @@
 
         private static readonly Logger _logger = new ();
         
+        public static bool TryFind<T>(out List<T> result, bool logging = true) where T : Component
+        {
+            result = Object.FindObjectsByType<T>(FindObjectsSortMode.None).ToList();
+
+            var anyComponents = result.Any();
+            
+            if (anyComponents && logging)
+            {
+                _logger.LogWarning($"No components of type {typeof(T).Name} found");
+            }
+            
+            return anyComponents;
+        }
     }
 }
