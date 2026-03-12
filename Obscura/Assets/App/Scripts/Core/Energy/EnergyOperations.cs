3320f654d25992f28e1cd3b3118fc18b0edf5d12
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-12 11:12:14 +0300

    Add energy entity field and its assignment inside constructor

--- Obscura/Assets/App/Scripts/Core/Energy/EnergyOperations.cs
+++ Obscura/Assets/App/Scripts/Core/Energy/EnergyOperations.cs
@@ -1,12 +1,16 @@
-﻿namespace App.Scripts.Core.Energy
+﻿using App.Scripts.Core.Storage;
+
+namespace App.Scripts.Core.Energy
 {
     public class EnergyOperations
     {
         private EnergyConfig _energyConfig;
+        private Storage.Entities.Energy _energyEntity;
         
         public EnergyOperations(EnergyConfig energyConfig)
         {
             _energyConfig = energyConfig;
+            EntitiesStorage.Instance.TryGet(out _energyEntity);
         }
     }
 }
