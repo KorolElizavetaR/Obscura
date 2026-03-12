f6fd72f457ac525aec4d028f9d8fcfaf6adc4221
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-12 12:11:04 +0300

    Add fields to manage energy info: texts (energyCount, timeToInrease), energyConfig and energyEntity

--- Obscura/Assets/App/Scripts/Core/UI/Labels/EnergyLabel.cs
+++ Obscura/Assets/App/Scripts/Core/UI/Labels/EnergyLabel.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using App.Scripts.Core.Energy;
+using TMPro;
+using UnityEngine;
 
 namespace App.Scripts.Core.UI.Labels
 {
     public class EnergyLabel : MonoBehaviour
     {
+        [SerializeField] private TextMeshPro _energyCount;
+        [SerializeField] private TextMeshPro _timeToIncrease;
         
+        private EnergyConfig _energyConfig;
+        private Storage.Entities.Energy _energyEntity;
     }
 }
