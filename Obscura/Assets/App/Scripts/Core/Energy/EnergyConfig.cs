6c4b9a26c2bf42a6a6456bf865443adbdb183f1b
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-12 11:35:47 +0300

    Change default value for recover energy speed to 600 seconds

--- Obscura/Assets/App/Scripts/Core/Energy/EnergyConfig.cs
+++ Obscura/Assets/App/Scripts/Core/Energy/EnergyConfig.cs
@@ -5,7 +5,7 @@ namespace App.Scripts.Core.Energy
     public class EnergyConfig : ScriptableObject
     {
         [SerializeField] private int _maxCount = 5;
-        [SerializeField] private float _recoverSpeed = 300f;
+        [SerializeField] private float _recoverSpeed = 600f;
         [SerializeField] private bool _resetOnStart = false;
 
         public int MaxCount => _maxCount;
