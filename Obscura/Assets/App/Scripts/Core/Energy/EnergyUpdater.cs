7b30c3c76b65f7010a3fa4895bc8dd9bf35f91a5
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-12 11:03:52 +0300

    Add _energyEntity field and its assignment

--- Obscura/Assets/App/Scripts/Core/Energy/EnergyUpdater.cs
+++ Obscura/Assets/App/Scripts/Core/Energy/EnergyUpdater.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using App.Scripts.Core.Storage;
+using UnityEngine;
 
 namespace App.Scripts.Core.Energy
 {
     public class EnergyUpdater : MonoBehaviour
     {
         [SerializeField] private EnergyConfig _energyConfig;
+        private Storage.Entities.Energy _energyEntity;
+        
+        protected virtual void Awake()
+        {
+            EntitiesStorage.Instance.TryGet(out _energyEntity);
+        }
     }
 }
